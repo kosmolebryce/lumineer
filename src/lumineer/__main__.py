a6a920fe918382bf710db05d9f29f937f260ab17
--global <--global>
2024-08-27 18:33:00 -0500

    Update: launcher icons bring open application instance to the fore

--- src/lumineer/__main__.py
+++ src/lumineer/__main__.py
@@ -1,9 +1,12 @@
-# `src/lumineer/__main__.py`
+import logging
+import os
+import psutil
 import sys
 from PyQt6.QtWidgets import (QApplication, QMainWindow, QWidget, QHBoxLayout, 
-                             QPushButton, QVBoxLayout, QLabel)
-from PyQt6.QtCore import Qt, QSize, QEvent, QPoint
+                             QPushButton, QVBoxLayout, QLabel, QMessageBox)
+from PyQt6.QtCore import Qt, QSize, QEvent, QPoint, QTextStream
 from PyQt6.QtGui import QFont, QKeySequence, QMouseEvent, QScreen
+from PyQt6.QtNetwork import QLocalServer, QLocalSocket
 from appdirs import user_data_dir
 from pathlib import Path
 
@@ -12,6 +15,10 @@ APP_NAME = "Lumineer"
 APP_AUTHOR = "kosmolebryce"
 APP_DATA_DIR = Path(user_data_dir(APP_NAME, APP_AUTHOR))
 ALIGHT_DIR = APP_DATA_DIR / "Alight"
+SERVER_NAME = "LumineerLauncherServer"
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 class LumineerLauncher(QMainWindow):
     def __init__(self):
@@ -20,14 +27,15 @@ class LumineerLauncher(QMainWindow):
         QApplication.instance().installEventFilter(self)
         self.oldPos = self.pos()
         self.applicationSupportsSecureRestorableState()
+        self.sub_apps = {}
 
     def initUI(self):
         self.setWindowTitle('Lumineer')
         self.setWindowFlags(Qt.WindowType.FramelessWindowHint | Qt.WindowType.WindowStaysOnTopHint)
-        
+
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
-        
+
         main_layout = QVBoxLayout(central_widget)
         main_layout.setSpacing(0)
         main_layout.setContentsMargins(0, 0, 0, 0)
@@ -80,7 +88,7 @@ class LumineerLauncher(QMainWindow):
         button.setFixedSize(50, 50)
         button.setToolTip(tooltip)
         button.clicked.connect(function)
-        
+
         button.setStyleSheet("""
             QPushButton {
                 border: none;
@@ -91,76 +99,72 @@ class LumineerLauncher(QMainWindow):
                 background-color: #4E4E4E;
             }
         """)
-        
+
         return button
+
+    def launch_application(self, app_name, app_class, *args, **kwargs):
+        if app_name in self.sub_apps and self.sub_apps[app_name].isVisible():
+            self.sub_apps[app_name].show()
+            self.sub_apps[app_name].raise_()
+            self.sub_apps[app_name].activateWindow()
+        else:
+            app_instance = app_class(*args, **kwargs)
+            app_instance.show()
+            self.sub_apps[app_name] = app_instance
 
     def launch_flash(self):
         from lumineer.flash.main import FlashcardApp
-        self.flash_app = FlashcardApp()
-        self.flash_app.show()
+        self.launch_application("flash", FlashcardApp)
 
     def launch_scholar(self):
         from lumineer.scholar.main import ManagyrApp, Managyr
         manager = Managyr()
-        self.scholar_app = ManagyrApp(manager)
-        self.scholar_app.show()
+        self.launch_application("scholar", ManagyrApp, manager)
 
     def launch_spectacle(self):
-        from lumineer.spectacle.main import NMRAnalysisHelper, NMRAnalyzerApp
-        self.spectacle_app = NMRAnalyzerApp()
-        self.spectacle_app.show()
+        from lumineer.spectacle.main import NMRAnalyzerApp
+        self.launch_application("spectacle", NMRAnalyzerApp)
 
     def launch_alight(self):
-        from lumineer.alight.main import AlightGUI, KnowledgeNode
+        from lumineer.alight.main import AlightGUI
         ALIGHT_DIR.mkdir(parents=True, exist_ok=True)
-        # db_path = ALIGHT_DIR / ALIGHT_DB_FILE
-        self.alight_app = AlightGUI()
-        self.alight_app.show()
+        self.launch_application("alight", AlightGUI)
+
+        return False
 
     def position_window(self):
         screen = QApplication.primaryScreen()
         screen_size = screen.geometry()
         size = self.geometry()
-        
+
         x = screen_size.width() - size.width() - 10
         y = screen_size.height() - size.height() - 100
-        
+
         self.move(x, y)
 
     def eventFilter(self, obj, event):
         if event.type() == QEvent.Type.KeyPress:
             key = event.key()
             modifiers = event.modifiers()
-            
+
             if (modifiers & Qt.KeyboardModifier.ControlModifier or modifiers & Qt.KeyboardModifier.MetaModifier) and key == Qt.Key.Key_Q:
                 self.close()
                 return True
-            
+
             if (modifiers & Qt.KeyboardModifier.ControlModifier or modifiers & Qt.KeyboardModifier.MetaModifier) and key == Qt.Key.Key_W:
                 if hasattr(self, 'scholar_app') and self.scholar_app.isVisible():
                     self.scholar_app.close()
                     return True
                 elif obj == self:
                     return True
-                    
+
         return super().eventFilter(obj, event)
 
     def closeEvent(self, event):
-        # List of possible sub-applications
-        sub_apps = ['flash_app', 'scholar_app', 'spectacle_app', 'alight_app']
-        
-        # Perform cleanup operations
-        for app_name in sub_apps:
-            if hasattr(self, app_name):
-                app = getattr(self, app_name)
-                if hasattr(app, 'isVisible') and app.isVisible():
-                    app.close()
-        
-        # Accept the close event
+        for app in self.sub_apps.values():
+            if app.isVisible():
+                app.close()
         event.accept()
-        
-        # Ensure the application quits
-        QApplication.instance().quit()
 
     def applicationSupportsSecureRestorableState(self):
         return True
@@ -181,7 +185,20 @@ class LumineerLauncher(QMainWindow):
         super().showEvent(event)
         self.ensure_on_top()
 
+def is_already_running():
+    current_process = psutil.Process()
+    for process in psutil.process_iter(['name', 'cmdline']):
+        if process.info['name'] == current_process.name() and \
+           process.info['cmdline'] == current_process.cmdline() and \
+           process.pid != current_process.pid:
+            return True
+    return False
+
 def main():
+    if is_already_running():
+        print("Lumineer is already running.")
+        sys.exit(0)
+
     app = QApplication(sys.argv)
     launcher = LumineerLauncher()
     launcher.show()
